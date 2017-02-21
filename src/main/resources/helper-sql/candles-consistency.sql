f8c58fc4d420219f2f3201a06875168322eecf41
Иван <NikolaychukIvan@yandex.ru>
2017-02-21 08:27:58 +0300

    Consistancy check

--- src/main/resources/helper-sql/candles-consistency.sql
+++ src/main/resources/helper-sql/candles-consistency.sql
@@ -5,6 +5,15 @@ join company_candle on candle.id = company_candle.candles_id
 group by company_name
 order by count(id)
 
+-- or
+
+select max(number_of_stocks), min(number_of_stocks) from (
+select count(id) as number_of_stocks, company_name from candle
+join company_candle on candle.id = company_candle.candles_id
+group by company_name
+order by number_of_stocks
+) a
+
 -- compare avg and max open
 select company_name, avg(open), max(open) from candle
 join company_candle on candle.id = company_candle.candles_id
