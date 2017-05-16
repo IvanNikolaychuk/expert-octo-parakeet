6aa2fba327d2333a39efd35f90f1fe20ce1bcb7a
Иван <NikolaychukIvan@yandex.ru>
2017-05-16 06:54:13 +0300

    changes

--- src/main/resources/helper-sql/analyser.sql
+++ src/main/resources/helper-sql/analyser.sql
@@ -1,13 +1,6 @@
--- how much performance has business type in this month .
-select * from company_growth_statistic_data
-join company on company_name = name
-where business_type='PHARMACEUTICS' and from_date like '2017-02-01%';
-
--- what business is most profitable now
-select business_type, sum(percentage_profit) / count(id) as profit_per_company
-from company_growth_statistic_data
-  join company on company_name = name
-where from_date like '2017-02-01%'
-group by business_type
-having count(id) >= 5
-order by profit_per_company desc;
+-- candle analyser
+SELECT *
+FROM candle
+  JOIN company_candle ON candle.id = company_candle.candles_id
+WHERE pattern != 'NONE'
+ORDER BY date DESC
