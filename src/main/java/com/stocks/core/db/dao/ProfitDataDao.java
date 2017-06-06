6ffdd305bccbf315282049f51531aecaa88b3af3
ivnikolaychuk <ivan.nikolaychuk@checkout.ru>
2017-06-06 08:51:06 +0300

    Revert: tmp (dbdd93191d8707dcf6b5d3fa1d6d5108721433b0)

--- src/main/java/com/stocks/core/db/dao/ProfitDataDao.java
+++
@@ -1,9 +0,0 @@
-package com.stocks.core.db.dao;
-
-import com.stocks.core.db.entity.ProfitData;
-
-/**
- * @author Ivan Nikolaichuk
- */
-public class ProfitDataDao extends AbstractDao<ProfitData> {
-}
--- src/main/java/com/stocks/core/db/dao/strongbull/ExpAverageCandleDao.java
+++ src/main/java/com/stocks/core/db/dao/strongbull/ExpAverageCandleDao.java
@@ -1,20 +1,7 @@
 package com.stocks.core.db.dao.strongbull;
 
 import com.stocks.core.db.dao.AbstractDao;
-import com.stocks.core.db.entity.company.Company;
 import com.stocks.core.db.entity.statistic.stongbull.ExpAverageCandle;
-import com.stocks.core.db.helper.HibernateUtils;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 
 public class ExpAverageCandleDao extends AbstractDao<ExpAverageCandle> {
-
-    public ExpAverageCandle findByCandleId(int candleId) {
-        try (SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            return (ExpAverageCandle) session.createCriteria(ExpAverageCandle.class)
-                    .add(Restrictions.eq("candle.id", candleId))
-                    .uniqueResult();
-        }   }
 }
